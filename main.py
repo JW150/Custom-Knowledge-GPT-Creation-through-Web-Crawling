390305553f400f04ee3a71c077885c1c4fc17622
JW150 <jw150@illinois.edu>
2024-12-08 20:25:28 -0500

    [Dev] wrap some code into functions

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ from llm import llm_generate, generate_prompt_with_knowledge
 
 load = False
 
+def save_report(text):
+    relative_path_to_target = os.path.join('.', 'files_storage', 'report_files')
+    target_directory = os.path.abspath(relative_path_to_target)
+    current_time = datetime.now()
+    file_name = "report" + current_time.strftime("%Y%m%d%H%M%S") + ".md"
+    file_path = os.path.join(target_directory, file_name)
+
+    with open(file_path, "w", encoding='utf-8') as file:
+        file.write(text)
+
+    os.chdir(target_directory)
+    convert_command = f"md-to-pdf {file_name}"  
+    subprocess.run(convert_command, shell=True)
+
 def analyze_website(url):
 
     original_dir = os.getcwd()
@@ -16,49 +30,23 @@ def analyze_website(url):
 
     rag_db = load_documents('output-1.json', load)
 
+	# here is an example of using RAG system for LLM to do product research
     product_description = llm_generate(SYS_PROMPT, generate_prompt_with_knowledge(USR_PROMPT1, rag_db, "company Description, product usecase"))
-    
-    usr_prompt2 = "\nPlease perform Target Market Analysis (demographics, psychographics, behavior) given the following content. Your response needs to be in markdown format. Always start from second level (##) heading\n\n" \
-    + product_description + "".join([item + "\n" for item in retrieve_info(rag_db, "Target Market Analysis, Product Description")])
+    target_market = llm_generate(SYS_PROMPT, generate_prompt_with_knowledge(USR_PROMPT2 + product_description, rag_db, "Target Market Analysis, Product Description"))
+    competitive_analysis = llm_generate(SYS_PROMPT, generate_prompt_with_knowledge(USR_PROMPT3 + product_description, rag_db, "Product details, features, Invention, new and different"))
+    pricing = llm_generate(SYS_PROMPT, generate_prompt_with_knowledge(USR_PROMPT4 + product_description, rag_db, "Pricing"))
 
-    target_market = llm_generate(SYS_PROMPT, usr_prompt2)
-
-    usr_prompt3 = "\nPlease perform detailed Competitive Analysis given the following content.You need to include product details. Your response needs to be in markdown format. Always start from second level (##) heading\n\n" \
-    + product_description + "".join([item + "\n" for item in retrieve_info(rag_db, "Product details, features, Invention, new and different")])
-
-    competitive_analysis = llm_generate(SYS_PROMPT, usr_prompt3)
-
-    usr_prompt4 = "\nPlease perform Pricing Strategy Analysis given the following content. Your response needs to be in markdown format. Always start from second level (##) heading\n\n" \
-    + product_description + "".join([item + "\n" for item in retrieve_info(rag_db, "Pricing")])
-
-    pricing = llm_generate(SYS_PROMPT, usr_prompt4)
-
-    usr_prompt5 = """\nPlease provide a short executive summary of the following paragraphs into one short paragraph and include concrete conclusion judegment. start with section title "Conclusion" and your response needs to be in markdown format. Always start from second level (##) heading\n\n""" \
+    final_usr_prompt = """\nPlease provide a short executive summary of the following paragraphs into one short paragraph and include concrete conclusion judegment. start with section title "Conclusion" and your response needs to be in markdown format. Always start from second level (##) heading\n\n""" \
     + product_description + competitive_analysis + target_market + pricing
 
-    summary = llm_generate(SYS_PROMPT, usr_prompt5) 
-
+    summary = llm_generate(SYS_PROMPT, final_usr_prompt) 
     final_report = "# Research Report\n" + product_description + "\n" + competitive_analysis + "\n" + target_market + "\n" + pricing + "\n" + summary 
 
-    relative_path_to_target = os.path.join('..', 'files_storage', 'report_files')
-    target_directory = os.path.abspath(relative_path_to_target)
-    current_time = datetime.now()
-    file_name = "report" + current_time.strftime("%Y%m%d%H%M%S") + ".md"
-    pdf_name = "report" + current_time.strftime("%Y%m%d%H%M%S") + ".pdf"
-    file_path = os.path.join(target_directory, file_name)
-    with open(file_path, "w", encoding='utf-8') as file:
-        file.write(final_report)
-
-    os.chdir(target_directory)
-    convert_command = f"md-to-pdf {file_name}"  
-
-    subprocess.run(convert_command, shell=True)
-
+    save_report(final_report)
     os.chdir(original_dir)
-    return final_report + f"\n\n[Download Report PDF](http://localhost:8080/report_files/{pdf_name})"
 
-analyze_website("https://www.rabbit.tech/")
+# analyze_website("https://www.rabbit.tech/")
 
-# SYS_PROMPT = "You are a world class analyst. You are very senstive to numbers since numbers are important. You have strong Analytical Skills and always paying Attention to Details. You use Problem-Solving techniques and Critical Thinking when needed."
-# usr_prompt1 = "\nPlease write a couple sentences short Product Descriptions given the following content."
-# product_description = llm_generate(SYS_PROMPT, usr_prompt1)
+SYS_PROMPT = "You are a world class analyst. You are very senstive to numbers since numbers are important. You have strong Analytical Skills and always paying Attention to Details. You use Problem-Solving techniques and Critical Thinking when needed."
+usr_prompt1 = "\nPlease write a couple sentences short Product Descriptions given the following content."
+product_description = llm_generate(SYS_PROMPT, usr_prompt1)
--- prompt.py
+++ prompt.py
@@ -2,3 +2,9 @@ SYS_PROMPT = "You are a world class analyst. You are very senstive to numbers si
 
 # Your Prompt here
 USR_PROMPT1 = "\nPlease write a couple sentences short Product Descriptions given the following content. Note that Title for this section needs to include the company name. Explain what the product is in one sentence without detailes.Your response needs to be in markdown format. Always start from second level (##) heading\n\n"
+
+USR_PROMPT2 = "\nPlease perform Target Market Analysis (demographics, psychographics, behavior) given the following content. Your response needs to be in markdown format. Always start from second level (##) heading\n\n"
+
+USR_PROMPT3 = "\nPlease perform detailed Competitive Analysis given the following content.You need to include product details. Your response needs to be in markdown format. Always start from second level (##) heading\n\n"
+
+USR_PROMPT4 = "\nPlease perform Pricing Strategy Analysis given the following content. Your response needs to be in markdown format. Always start from second level (##) heading\n\n"
